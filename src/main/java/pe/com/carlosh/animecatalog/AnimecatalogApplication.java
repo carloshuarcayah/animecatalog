d08d336a86d701f7c1ba013ed6bb8aeb19bbe406
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-03-09 16:19:16 -0500

    chore: initial project setup with docker

---
+++ src/main/java/pe/com/carlosh/animecatalog/AnimecatalogApplication.java
@@ -0,0 +1,13 @@
+package pe.com.carlosh.animecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimecatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimecatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/com/carlosh/animecatalog/AnimecatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.carlosh.animecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimecatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
