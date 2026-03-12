12b49ccfd5ed8e0dee2e75a664c639ddf17376e8
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-03-11 21:33:23 -0500

    feat(author): add repository

---
+++ src/main/java/pe/com/carlosh/animecatalog/author/AuthorRepository.java
@@ -0,0 +1,21 @@
+package pe.com.carlosh.animecatalog.author;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Page<Author> findByActiveTrue(Pageable pageable);
+    Optional<Author> findByIdAndActiveTrue(Long id);
+
+    @Query("SELECT a FROM Author a WHERE a.active = true AND " +
+            "(LOWER(a.firstName) LIKE LOWER(CONCAT('%', :name, '%')) OR " +
+            "LOWER(a.lastName) LIKE LOWER(CONCAT('%', :name, '%')))")
+    Page<Author> searchByName(@Param("name") String name, Pageable pageable);
+
+    boolean existsByFirstNameIgnoreCaseAndLastNameIgnoreCase(String firstName, String lastName);
+}
