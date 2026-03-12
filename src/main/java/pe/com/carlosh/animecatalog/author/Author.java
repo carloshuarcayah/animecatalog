8fb27e6eb49654ad2317477e36ac4dc88091bba1
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-03-11 21:32:46 -0500

    feat(author): add entity

---
+++ src/main/java/pe/com/carlosh/animecatalog/author/Author.java
@@ -0,0 +1,63 @@
+package pe.com.carlosh.animecatalog.author;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Table(name = "authors")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String firstName;
+
+    @Column(nullable = false, length = 100)
+    private String lastName;
+
+    @Column
+    private LocalDate birthDate;
+
+    @Column(length = 100)
+    private String nationality;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    private Boolean active;
+
+    public Author(String firstName, String lastName, LocalDate birthDate, String nationality) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.nationality = nationality;
+        this.active = true;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
