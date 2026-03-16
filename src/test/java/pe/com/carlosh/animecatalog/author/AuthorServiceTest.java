c3d1636ed97dd8c3582d1e454b79bf81fd9837aa
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-03-16 12:45:55 -0500

    test(author): add unit test for findAll

--- src/test/java/pe/com/carlosh/animecatalog/author/AuthorServiceTest.java
+++ src/test/java/pe/com/carlosh/animecatalog/author/AuthorServiceTest.java
@@ -11,7 +11,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import pe.com.carlosh.animecatalog.author.dto.AuthorRequestDTO;
 import pe.com.carlosh.animecatalog.author.dto.AuthorResponseDTO;
 
 import java.time.LocalDate;
