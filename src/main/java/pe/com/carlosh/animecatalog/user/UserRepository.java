f7e64cf8290263d59d82ea2e7f8c0c059d32562f
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-03-20 16:15:53 -0500

    feat(user): create repository

---
+++ src/main/java/pe/com/carlosh/animecatalog/user/UserRepository.java
@@ -0,0 +1,10 @@
+package pe.com.carlosh.animecatalog.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+}
