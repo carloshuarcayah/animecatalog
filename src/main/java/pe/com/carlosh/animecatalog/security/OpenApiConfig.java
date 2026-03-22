4a59821b25ca9df5c89b791c70a043ad89185917
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-03-22 11:27:10 -0500

    feat(security): add authorize buttom to swagger

--- src/main/java/pe/com/carlosh/animecatalog/security/OpenApiConfig.java
+++ src/main/java/pe/com/carlosh/animecatalog/security/OpenApiConfig.java
@@ -1,6 +1,9 @@
 package pe.com.carlosh.animecatalog.security;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,11 +15,21 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        // Esto obliga a Swagger a usar la URL de Railway con HTTPS
+        final String securitySchemeName = "bearerAuth";
+
         Server productionServer = new Server();
         productionServer.setUrl("https://animecatalog-production.up.railway.app");
         productionServer.setDescription("Servidor de Producción en Railway");
 
-        return new OpenAPI().servers(List.of(productionServer));
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .servers(List.of(productionServer));
     }
 }
